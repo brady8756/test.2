580fefac33f10208fa4ba6fc0c37ddead7634e5c
brady8756 <95857599+brady8756@users.noreply.github.com>
2021-12-09 22:55:53 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
-gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
+gas = st.number_input('油量的資料收集:油箱滿是10格 <=')
 speed = st.number_input('車速的資料收集:限速100 =>')
 temp = st.number_input('溫度的資料收集:限溫30 =>')
 轉速 = st.number_input('轉速資料的收集:限轉12000 =>')
